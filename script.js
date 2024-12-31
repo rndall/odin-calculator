3648d64bfd438db0c9fa10a481eea729f4062617
rndall <96964503+rndall@users.noreply.github.com>
2024-12-31 18:35:52 +0800

    Change where number variables are converted to numbers
    Shows correct results when a number variable is "0".

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ const allClear = () => {
 
 const setNumberValues = () => {
 	const parts = prevDisplay.innerText.split(operator);
-	[firstVal, secondVal] = [+parts[0], +parts[1].slice(0, -1)];
+	[firstVal, secondVal] = [parts[0], parts[1].slice(0, -1)];
 };
 
 const populateDisplay = (button) => {
@@ -61,7 +61,7 @@ const populateDisplay = (button) => {
 			setNumberValues();
 
 			if (firstVal && operator && secondVal) {
-				currDisplay.textContent = operate(operator, firstVal, secondVal);
+				currDisplay.textContent = operate(operator, +firstVal, +secondVal);
 				operator = firstVal = secondVal = null;
 			}
 		}
